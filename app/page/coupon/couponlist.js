a2a2973083e80a37c2772e3a3676ebb9210e3f32
genie <genie88@163.com>
2016-02-02 13:00:59 +0800

    增加我的优惠券UI设计

--- app/page/coupon/couponlist.js
+++ app/page/coupon/couponlist.js
@@ -56,17 +56,31 @@ var Market = React.createClass({
   _renderListItem:function(rowData){
     return (
       <View style={{padding:15,backgroundColor:'white',marginBottom:10}}>
-        <View style={{flexDirection:'row'}}>
-          <Text style={{color:'#424242'}}>text1</Text>
-          <Text style={{color:'#424242',marginLeft:20,}}>text2</Text>
-        </View>
+        <View style={styles.stamp, styles.stamp01, {height: 120, flexDirection:'row', backgroundColor:'#D24161',overflow:'hidden'}}>
+            <View style={styles.par, {flex: 2, borderRightWidth: 1, borderRightColor: 'rgb(255,255,255,.8)', borderStyle: 'dashed', padding: 10}}>
+              <Text style={styles.sup, {color:'#fff', fontSize: 14,marginTop: 5}}>长沙景和食品有限公司</Text>
+              <Text style={styles.sign, {fontSize:20, color:'#fff', marginTop: 10}}>￥
+                <Text style={styles.amount, {fontSize:35, color:'#fff',fontWeight: 'bold'}}>50.00</Text>
+                <Text style={styles.sub, {fontSize:14, color:'#fff'}}>  优惠券</Text>
+              </Text>
+              <Text style={styles.desc, {fontSize:14, color:'#fff', marginTop: 10}}>订单满100.00元</Text>
+            </View>
+            <View style={styles.copy, {flex: 1,padding:10}}>
+              <Text style={{fontSize:20, color:'#fff', marginTop: 15,fontWeight: '300'}}>副券</Text>
+              <Text style={styles.duedate, {fontSize:14, color:'#fff', marginTop: 15,fontWeight: 'bold'}}>过期时间: 2015-08-13</Text>
+            </View>
+            <View style={styles.ibg, {
+                position: 'absolute', left: 20, top: 45, height: 190, width: 390, 
+                backgroundColor: 'rgba(255, 255, 255, .15)', transform: [{rotate: '-30deg'}]
+              }}></View>
+            </View>
       </View>
       );
   },
 
   render: function() {
     if(!this.state.loaded){
-        return <Loading loadingtext='正在加载红包列表数据...'/>
+        return <Loading loadingtext='正在加载卡券列表数据...'/>
     };
     return (
      <View style={styles.container}>
@@ -82,6 +96,73 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  stamp: {
+    flex: 1,
+    height: 440,
+    paddingVertical: 0,
+    paddingHorizontal: 10,
+    marginBottom: 50,
+    position: 'relative',
+    overflow: 'hidden',
+    backgroundColor: '#F39B00'
+  },
+  // .stamp:before {
+  //   content: '';
+  //   position: absolute;
+  //   top:0;
+  //   bottom:0;
+  //   left:10px;
+  //   right:10px;
+  //   z-index: -1;
+  // }
+  // ibg: {
+  //   position: 'absolute',
+  //   left: 20,
+  //   top: 45,
+  //   height: 190,
+  //   width: 390,
+  //   backgroundColor: 'rgba(255, 255, 255, .15)',
+  //   transform: [{rotate: '-30deg'}]
+  // },
+  par: {
+    paddingVertical: 16,
+    paddingHorizontal: 15,
+    width: 220,
+    borderStyle: 'dashed',
+    borderRightWidth: 2,
+    borderRightColor: 'rgba(255, 255, 255, .3)'
+  },
+  // sup: {
+  //   color:'#fff',
+  //   fontSize: 16,
+  //   lineHeight: 21
+  // },
+  // amount: {
+  //   fontSize: 40,
+  //   color: '#fff',
+  //   marginRight: 5,
+  //   lineHeight: 65
+  // },
+  // sign: {
+  //   fontSize: 24
+  // },
+  // sub: {
+  //   position: 'relative',
+  //   top:-5,
+  //   color: 'rgba(255, 255, 255, .8)'
+  // },
+  // copy: {
+  //   paddingVertical: 21,
+  //   paddingHorizontal: 14,
+  //   width:100,
+  //   fontSize: 30,
+  //   color: 'rgb(255,255,255)',
+  //   textAlign: 'center'
+  // },
+  // duedate: {
+  //   fontSize: 16,
+  //   marginTop: 15
+  // }
 });
 
 module.exports = Market;
